30cae9966f789abaae3652d6d3f01b5dd427a913
HAROUN <haroundominique@gmail.com>
2022-12-03 02:45:41 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        for(int i = 0; i < 100; i++){
+            counter++;
+            if(counter%3==0 && counter%5==0){
+                System.out.println("FizzBuzz");
+            } else if (counter%3==0) {
+                System.out.println("Fizz");
+            } else if (counter%5==0) {
+                System.out.println("Buzz");
+            } else{
+                System.out.println(counter);
+            }
+        }
+    }
+}
