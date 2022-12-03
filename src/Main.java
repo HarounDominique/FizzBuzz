4a8863f87d91eaa09c57699b4196ea2acada567f
HAROUN <haroundominique@gmail.com>
2022-12-03 16:56:56 +0100

    adding bazz if its divisible by 7

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,17 @@ public class Main {
     public static void main(String[] args) {
         int counter = 0;
 
-        for(int i = 0; i < 100; i++){
+        for (int i = 0; i < 100; i++) {
             counter++;
-            if(counter%3==0 && counter%5==0){
+            if (counter % 3 == 0 && counter % 5 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (counter%3==0) {
+            } else if (counter % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (counter%5==0) {
+            } else if (counter % 5 == 0) {
                 System.out.println("Buzz");
-            } else{
+            } else if (counter % 7 == 0) {
+                System.out.println("Bazz");
+            } else {
                 System.out.println(counter);
             }
         }
